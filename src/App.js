bf5edcd230ecf4b22b862a290804e83219a77c0b
blessedmadukoma <blessedmadukoma@gmail.com>
2023-04-13 09:47:16 +0100

    base layout set up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
+import Articles from "./components/Articles";
+import User from "./components/User";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Skeletons</h1>
       </header>
+
+      <div className="content">
+        <Articles />
+        <User />
+      </div>
     </div>
   );
 }
---
+++ src/components/Articles.js
@@ -0,0 +1,9 @@
+const Articles = () => {
+  return (
+    <div className="article">
+      <h2>Article Details</h2>
+    </div>
+  );
+};
+
+export default Articles;
