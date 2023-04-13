4298cc045137ba3e187ff403a5ed775e00a54b3e
blessedmadukoma <blessedmadukoma@gmail.com>
2023-04-13 22:16:33 +0100

    added components

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,7 +1,40 @@
+import { useState, useEffect } from "react";
+
 const Articles = () => {
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    // delay the fetching of data to show the skeleton
+    setTimeout(async () => {
+      const result = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await result.json();
+      setArticles(data);
+    }, 5000);
+  });
+
   return (
     <div className="article">
       <h2>Article Details</h2>
+
+      {articles && (
+        <div className="articles">
+          {articles.map((article) => (
+            <div className="article" key={article.id}>
+              <h3>{article.title}</h3>
+              <p>{article.body}</p>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {!articles && (
+        <div className="skeleton">
+          loading...
+          {/* <div className="skeleton-avatar"></div>
+          <div className="skeleton-author"></div>
+          <div className="skeleton-details"></div> */}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,41 @@
+import { useState, useEffect } from "react";
+
 const User = () => {
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    // delay the fetching of data to show the skeleton
+    setTimeout(async () => {
+      const result = await fetch(
+        "https://jsonplaceholder.typicode.com/users/1"
+      );
+      const data = await result.json();
+      setProfile(data);
+    }, 5000);
+  });
+
   return (
     <div className="user">
       <h2>User Details</h2>
+
+      {profile && (
+        <div className="profile">
+          <h3>{profile.username}</h3>
+          <p>{profile.email}</p>
+          <a href={profile.website} target="_blank">
+            {profile.website}
+          </a>
+        </div>
+      )}
+
+      {!profile && (
+        <div className="skeleton">
+          loading...
+          {/* <div className="skeleton-avatar"></div>
+          <div className="skeleton-author"></div>
+          <div className="skeleton-details"></div> */}
+        </div>
+      )}
     </div>
   );
 };
